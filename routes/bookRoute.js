91563a9436d37b23b00660ddefdca70e5422bab9
fikrisabiq <fikrisabiq@gmail.com>
2022-12-15 15:47:23 +0700

    l

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,13 +3,13 @@ import {
   getInfo,
   getBooks,
   saveBook,
-  deleteBook
-} from ('../controllers/bookController.js')
+  deleteBook,
+} from '../controllers/bookController.js';
 
 const router = express.Router();
 
 router.get('/api/books', getBooks);
-router.get('/api/info', getInfo)
+router.get('/api/info', getInfo);
 router.post('/api/books', saveBook);
 router.delete('/api/books', deleteBook);
 
