ed58a3dae7e221c4c932a02a883d9158eb4e3680
fikrisabiq <fikrisabiq@gmail.com>
2022-12-15 18:49:31 +0700

    terte

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const bookRoute = require('./routes/bookRoute');
 require('./services/cache');
 require('./model/Book');
 
