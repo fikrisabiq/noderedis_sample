1b8025bbbfe7d607656f29e666fdc9671f2c7275
fikrisabiq <81612058+fikrisabiq@users.noreply.github.com>
2022-12-15 14:18:04 +0700

    Update cache.js

--- services/cache.js
+++ services/cache.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
-// const keys = require("../config/keys");
 
 (async () => {
   const client = redis.createClient({
