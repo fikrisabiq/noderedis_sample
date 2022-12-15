7edfcc5e1f3a51372883b522d2ce5d1a943e1c40
fikrisabiq <fikrisabiq@gmail.com>
2022-12-15 15:40:29 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+import bookRoute from './routes/bookRoute';
 
 require('./services/cache');
 require('./model/Book');
